59d00aac88cf29e61e738633e6848b12287ed7c4
Sourmojit <sourmojitn098@gmail.com>
2026-06-14 01:37:23 +0530

    Prani Raksha - Initial deployment

---
+++ next.config.mjs
@@ -0,0 +1,21 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [],
+  },
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff',
+          },
+        ],
+      },
+    ]
+  },
+}
+
+export default nextConfig
